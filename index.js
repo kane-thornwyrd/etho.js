f91288f3b31b1ac741048fdef69b7bd9bcf63bf0
Jean-cédric THÉROND <jean.cedric.t@gmail.com>
2014-07-11 07:51:47 +0200

    tries

--- index.js
+++ index.js
@@ -1,70 +1,76 @@
-var etho = etho || {};
+define('etho', [], function(){
 
-etho.instanceOf = function instanceOf(_obj, ancestor) {
-  while (_obj != null) { if (_obj == ancestor.prototype){ return true; }
-    _obj = _obj.__proto__;
-  }
-  return false;
-}
+  var etho = {};
 
-etho.forEach = function(obj, iterator, context) {
-  if (obj == null){ return;}
-  if (Array.prototype.forEach && obj.forEach === Array.prototype.forEach) {
-    obj.forEach(iterator, context);
-  } else if (obj.length === +obj.length) {
-    for (var i = 0, l = obj.length; i < l; i++) {
-      if (iterator.call(context, obj[i], i, obj) === {}){ return;}
+  etho.instanceOf = function instanceOf(_obj, ancestor) {
+    while (_obj != null) { if (_obj == ancestor.prototype){ return true; }
+      _obj = _obj.__proto__;
     }
-  } else {
-    for (var key in obj) {
-      if (Object.prototype.hasOwnProperty.call(obj, key)) {
-        if (iterator.call(context, obj[key], key, obj) === {}){ return;}
+    return false;
+  }
+
+  etho.forEach = function(obj, iterator, context) {
+    if (obj == null){ return;}
+    if (Array.prototype.forEach && obj.forEach === Array.prototype.forEach) {
+      obj.forEach(iterator, context);
+    } else if (obj.length === +obj.length) {
+      for (var i = 0, l = obj.length; i < l; i++) {
+        if (iterator.call(context, obj[i], i, obj) === {}){ return;}
+      }
+    } else {
+      for (var key in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, key)) {
+          if (iterator.call(context, obj[key], key, obj) === {}){ return;}
+        }
       }
     }
-  }
-};
+  };
 
-etho.x = function ethoX(className, parent, child){
+  etho.x = function ethoX(className, parent, child){
 
-  if(!child){
-    child = parent;
-    parent = Object;
-  }
-
-  var parentMethod = function parentInvoke(method){
-    if(typeof this.parent[method] !== 'undefined'){
-      return this.parent[method].bind(this);
+    if(!child){
+      child = parent;
+      parent = Object;
     }
-    return null;
+
+    var parentMethod = function parentInvoke(method){
+      if(typeof this.parent[method] !== 'undefined'){
+        return this.parent[method].bind(this);
+      }
+      return null;
+    };
+    if ( parent.constructor == Function ){
+      child.prototype = new parent;
+      child.prototype.constructor = child;
+      child.prototype.parent = parent.prototype;
+      child.prototype.parent.parentMethod = parentMethod;
+    } else {
+      child.prototype = parent;
+      child.prototype.constructor = child;
+      child.prototype.parent = parent;
+    }
+
+    child.prototype.meta = {};
+
+    if(typeof className !== 'string'){
+      etho.forEach(className, function(value, key, list){
+        child.prototype.meta[key] = value;
+      });
+    } else {
+      child.prototype.meta.name = className;
+    }
+
+    return function prototypeEnrichment(newMethods){
+      etho.forEach(newMethods, function(value, key, list){
+        child.prototype[key] = value;
+      });
+      return child;
+    };
   };
-  if ( parent.constructor == Function ){
-    child.prototype = new parent;
-    child.prototype.constructor = child;
-    child.prototype.parent = parent.prototype;
-    child.prototype.parent.parentMethod = parentMethod;
-  } else {
-    child.prototype = parent;
-    child.prototype.constructor = child;
-    child.prototype.parent = parent;
-  }
 
-  child.prototype.meta = {};
+  return etho;
 
-  if(typeof className !== 'string'){
-    etho.forEach(className, function(value, key, list){
-      child.prototype.meta[key] = value;
-    });
-  } else {
-    child.prototype.meta.name = className;
-  }
-
-  return function prototypeEnrichment(newMethods){
-    etho.forEach(newMethods, function(value, key, list){
-      child.prototype[key] = value;
-    });
-    return child;
-  };
-}
+});
 
 
 var Foo = etho.x({
